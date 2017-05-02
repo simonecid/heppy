fa26b6135d2fe55225b24f58c25dbfb9daeb24a0
Simone Bologna <smnbologna@gmail.com>
2017-05-02 14:10:02 +0100

    New version of script

--- runHeppy.sh
+++ runHeppy.sh
@@ -4,10 +4,10 @@ SAVE_DEST="$(pwd)"
 
 mkdir output
 
-cd /software/sb17498/FCCSW
 source /software/sb17498/FCCSW/init.sh
-cd /software/sb17498/heppy
-source /software/sb17498/heppy/init.sh
+cp -r /software/sb17498/heppy .
+cd heppy
+source init.sh
 
 heppy ${SAVE_DEST}/output test/triggerRates_cfg.py
 
