5cc1d0d82c681839146c612b56914d79dfec681c
Simone Bologna <smnbologna@gmail.com>
2017-05-29 16:10:01 +0100

    Finished component. Some debug stuff is inside.

--- analyzers/triggerrates/JetTransformer.py
+++ analyzers/triggerrates/JetTransformer.py
@@ -3,13 +3,14 @@
 from heppy.framework.analyzer import Analyzer
 import collections
 from numpy.random import RandomState
+import pdb
 
 class JetTransformer  (Analyzer):
   '''Transforms a jet into another object with a certaing probability.
   
   Example: 
 
-  Let's suppose that a 10-20 GeV jet has a 5% prob to become an electron, and a 20-30% has a 1%. 
+  Let's suppose that a 10-20 GeV jet has a 5% prob to become an electron, and a 20-30 has a 1%. 
   We would have:
 
   jetToElectron = []
@@ -17,13 +18,15 @@ class JetTransformer  (Analyzer):
   jetToElectron.append(0.05) #10-20 GeV
   jetToElectron.append(0.01) #20-30 GeV
 
-  from heppy.analyzers.JetTransformer   import JetTransformer  
+  from heppy.analyzers.triggerrates.JetTransformer import JetTransformer  
   jetToElectronTrasformer = cfg.Analyzer(
     JetTransformer ,
+    jet_collection = 'jets',
     output_objects = 'electrons',
-    conversion_factors = jetToElectron,
+    conversion_factors = jetToElectron
   )
   
+  * jet_objects : input collection containing the jets
   * conversion_factors : jet-to-object conversion factors, first will be always ignored. It is at steps of 10 GeV.
   * output_objects : output collection which the jet will be stored in, i.e. the type of object the jet will be converted to
   
@@ -35,16 +38,15 @@ class JetTransformer  (Analyzer):
     self.rng.seed()
 
   def process(self, event):
-    '''event must contain
     
-    * self.cfg_ana.output_objects: collection of objects to be selected
-       These objects must be usable by the filtering function
-       self.cfg_ana.trigger_func.
-    '''
-    output_collection = getattr(event, self.cfg_ana.input_objects)
-    jets = event.jets
+    jets = getattr(event, self.cfg_ana.jet_collection)
+    output_collection = getattr(event, self.cfg_ana.output_objects)
+    conversion_factors = self.cfg_ana.conversion_factors
 
     jetIdx = 0
+
+    if len(jets) > 0:
+      pdb.set_trace()
 
     for jet in jets:
       jetPt = jet.pt()
@@ -52,9 +54,13 @@ class JetTransformer  (Analyzer):
         # Getting the corresponding index
         factorIndex = int(jet.pt()/10)
         # Checking if it must be converted
-        if self.rng.uniform(0, 1) < self.conversion_factors[factorIndex] : 
+        if factorIndex >= len(conversion_factors):
+          continue
+
+        if self.rng.uniform(0, 1) < conversion_factors[factorIndex] : 
           # It has to be converted: I will remove the object from the jet collection and add it to the output one
+          print "A jet with", jet.pt(), "has been converted to", self.cfg_ana.output_objects
           output_collection.append(jet)
-          del jet[jetIdx]
+          # del jets[jetIdx]
       
       jetIdx += 1
