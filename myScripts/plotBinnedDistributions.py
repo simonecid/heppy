85740aad9b0ff2a103af66bb1208fcb8cdd8c2f9
Simone Bologna <smnbologna@gmail.com>
2017-10-31 16:35:54 +0000

    binning and histograms

--- myScripts/plotBinnedDistributions.py
+++ myScripts/plotBinnedDistributions.py
@@ -6,20 +6,24 @@ from ROOT import TTree
 from ROOT import TCanvas
 from ROOT import TLegend
 
-convolutionFile = TFile("_binnedDistributions/cmsMatching_QCD_15_3000_L1TEGamma_GenJet/histograms.root")
+convolutionFile = TFile("_muonTriggerRate_EtaDistributionTest/lowPt_ConvolutionCurves/histograms.root")
 
 '''List of every distribution to plot'''
 distributionNames = [
-  "objectPtDistributionBinnedInMatchedObject",
-  "objectMatchedObjectPtRatioDistributionBinnedInMatchedObject",
+  #"objectPtDistributionBinnedInMatchedObject",
+  #"objectMatchedObjectPtRatioDistributionBinnedInMatchedObject",
+  "matchedObjectEtaDistributionBinnedInMatchedObject",
   "objectEtaDistributionBinnedInMatchedObject",
-  "deltaRDistributionBinnedInMatchedObject",
-  "deltaPtDistributionBinnedInMatchedObject"
+  #"deltaRDistributionBinnedInMatchedObject",
+  #"deltaPtDistributionBinnedInMatchedObject"
 ]
 
 '''Bins of binned distributions'''
 #ptBins = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60]
-ptBins = [0, 10, 20, 30, 40, 50, 60]
+#ptBins = [3, 5, 10, 20, 30, 40]
+ptBins = [1.5, 2.3, 3, 4, 5, 5.5]
+#ptBins = [20, 25, 30, 35, 40, 45, 50, 60, 70]
+
 
 '''Contains the distributions'''
 distributions = {}
