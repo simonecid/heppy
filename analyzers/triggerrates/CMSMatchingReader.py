2db6ad18bd29736131a4cc111776b129a2f6159d
Simone Bologna <smnbologna@gmail.com>
2017-10-31 16:24:57 +0000

    Added logic to get particle objects, now works better with only 1 particle

--- analyzers/triggerrates/CMSMatchingReader.py
+++ analyzers/triggerrates/CMSMatchingReader.py
@@ -1,9 +1,11 @@
 from heppy.framework.analyzer import Analyzer
-from heppy.particles.fcc.particle import Particle
+from heppy.particles.particle import Particle
 from heppy.particles.fcc.jet import Jet
 from heppy.particles.fcc.vertex import Vertex 
 from heppy.particles.fcc.met import Met
 import heppy.configuration
+from ROOT import TLorentzVector
+
 
 from heppy.particles.TriggerObject import TriggerObject
 
@@ -42,21 +44,41 @@ class CMSMatchingReader(Analyzer):
   
   def process(self, event):
 
-    gen_object = TriggerObject()
-    trigger_object = TriggerObject()
+    gen_object = Particle()
+    trigger_object = Particle()
     tree = event.input
     if hasattr(self.cfg_comp, "gen_object"):
 #      gen_object.id = getattr(tree, self.cfg_comp.gen_object + "_id")
-      gen_object._pt = getattr(tree, self.cfg_comp.gen_object + "_pt")
-      gen_object._eta = getattr(tree, self.cfg_comp.gen_object + "_eta")
-      gen_object._phi = getattr(tree, self.cfg_comp.gen_object + "_phi")
+      gen_object_pt = getattr(tree, self.cfg_comp.gen_object + "_pt")
+      gen_object_eta = getattr(tree, self.cfg_comp.gen_object + "_eta")
+      gen_object_phi = getattr(tree, self.cfg_comp.gen_object + "_phi")
+      gen_object = Particle()
+      gen_object._tlv = TLorentzVector()
+      gen_object._pid = 0
+      gen_object._charge = 0
+      gen_object._status = 1
+      gen_object._tlv.SetPtEtaPhiE(gen_object_pt, gen_object_eta, gen_object_phi, 0)
       event.gen_objects = [gen_object]
     if hasattr(self.cfg_comp, "trigger_object"):
 #      trigger_object.id = getattr(tree, self.cfg_comp.trigger_object + "_id")
-      trigger_object._pt = getattr(tree, self.cfg_comp.trigger_object + "_pt")
-      trigger_object._eta = getattr(tree, self.cfg_comp.trigger_object + "_eta")
-      trigger_object._phi = getattr(tree, self.cfg_comp.trigger_object + "_phi")
-      trigger_object.match = gen_object
-      trigger_object.deltaR2 = getattr(tree, "deltaR2", None)
-      if trigger_object.deltaR2 is None: trigger_object.deltaR2 = tree.dr**2
-      event.trigger_objects = [trigger_object]
+      trigger_object_pt = getattr(tree, self.cfg_comp.trigger_object + "_pt", None)
+      trigger_object_eta = getattr(tree, self.cfg_comp.trigger_object + "_eta", None)
+      trigger_object_phi = getattr(tree, self.cfg_comp.trigger_object + "_phi", None)
+      if trigger_object_pt is not None:
+        trigger_object = Particle()
+        trigger_object._tlv = TLorentzVector()
+        trigger_object._pid = 0
+        trigger_object._charge = 0
+        trigger_object._status = 1
+        trigger_object._tlv.SetPtEtaPhiE(trigger_object_pt, trigger_object_eta, trigger_object_phi, 0)
+        trigger_object.match = gen_object
+        trigger_object.deltaR2 = getattr(tree, "deltaR2", None)
+        trigger_object.quality = getattr(tree, "l1tMuon_qual", None)
+        if trigger_object.deltaR2 is None: trigger_object.deltaR2 = tree.dr**2
+        event.trigger_objects = [trigger_object]
+      else:
+        event.trigger_objects = []
+        self.cfg_comp.trigger_object = "NoObject"  
+    else:
+      event.trigger_objects = []
+      self.cfg_comp.trigger_object = "NoObject"
