2c37dba8f27c33a01cbbbb376e8f65b82219bf59
Simone Bologna <smnbologna@gmail.com>
2017-11-06 13:43:14 +0000

    Copied some stuff from muon workflow to generalise it

---
+++ genObjectToL1TObjectConvolutionCurves/binnedDistributionsCMS_L1TMuon_cfg.py
@@ -0,0 +1,369 @@
+import os
+import copy
+import heppy.framework.config as cfg
+from heppy.samples.mySamples import *
+import logging
+from heppy.framework.chain import Chain as Events
+from heppy.framework.services.tfile import TFileService
+from heppy.analyzers.triggerrates.Histogrammer import Histogrammer
+from heppy.framework.looper import Looper
+from heppy.analyzers.Matcher import Matcher
+from heppy.analyzers.Selector import Selector
+from heppy.analyzers.triggerrates.MatchedParticlesTreeProducer import MatchedParticlesTreeProducer
+from heppy.analyzers.triggerrates.MatchedObjectBinnedDistributions import MatchedObjectBinnedDistributions
+from heppy.analyzers.triggerrates.MatchedObjectBinnedCumulativeDistributions import MatchedObjectBinnedCumulativeDistributions
+from heppy.analyzers.triggerrates.CMSMatchingReader import CMSMatchingReader
+from heppy.analyzers.triggerrates.ObjectFinder import ObjectFinder
+from heppy.analyzers.triggerrates.HistogrammerCumulative import HistogrammerCumulative
+from heppy.framework.heppy_loop import _heppyGlobalOptions
+from math import sqrt
+
+# next 2 lines necessary to deal with reimports from ipython
+#logging.shutdown()
+#reload(logging)
+#logging.basicConfig(level=logging.WARNING)
+
+#Object name
+objectName = "L1T Muon"
+matchedObjectName = "Gen Muon"
+#sampleName = "l1tGenJetMatching_QCD_15_3000_NoPU_Phase1_L11Obj_To_GenJet_Match_ClosestDR_L1TEGamma_GenJet"
+sampleName = "l1tMuonGenMuonMatching_SingleMu_FlatPt_8to100_QualityCut_WQualityBranch"
+ptBins = [0, 1.5, 3, 5, 8, 11, 15, 20, 30, 40, 50, 70, 100, 140, 200]
+muonMinimumPtInBarrel = float(_heppyGlobalOptions["minimumPtInBarrel"])
+muonMinimumPtInEndcap = float(_heppyGlobalOptions["minimumPtInEndcap"])
+barrelEta = float(_heppyGlobalOptions["barrelEta"])
+
+if "binning" in _heppyGlobalOptions:
+  import ast
+  ptBins = ast.literal_eval(_heppyGlobalOptions["binning"])
+
+# Retrieving the sample to analyse
+
+#if specified in sample, a specific set will be used, otherwise the full set will be employed
+if "sample" in _heppyGlobalOptions:
+  sampleName = _heppyGlobalOptions["sample"]
+if sampleName == "all":
+  selectedComponents = [
+    cmsMatching_QCD_15_3000_L1TMuon_GenJet,
+    cmsMatching_QCD_15_3000_L1TEGamma_GenJet,
+    cmsMatching_QCD_15_3000_L1TTau_GenJet,
+  ]
+else:  
+  sample = globals()[sampleName]
+  selectedComponents = [
+    sample
+  ]
+
+# Defining pdgids
+
+source = cfg.Analyzer(
+  CMSMatchingReader,
+)
+
+
+tfile_service_1 = cfg.Service(
+  TFileService,
+  'tfile1',
+  fname='histograms.root',
+  option='recreate'
+)
+
+def pt (ptc):
+  return ptc.pt()
+
+def deltaPt(ptc):
+  return ptc.pt() - ptc.match.pt()
+
+def eta (ptc):
+  return ptc.eta()
+
+def phi (ptc):
+  return ptc.phi()
+
+def matchedParticlePt (ptc):
+  return ptc.match.pt()
+
+def matchedParticleEta (ptc):
+  return ptc.match.eta()
+
+def ptRatioWithMatched (ptc):
+  return ptc.pt()/ptc.match.pt()
+
+def deltaR (ptc):
+  return sqrt(ptc.deltaR2)
+
+def isMatched(ptc):
+  return ptc.match is not None
+
+def qualityCut(ptc):
+  return ptc.quality >=int(_heppyGlobalOptions["quality"])
+
+def quality(ptc):
+  return ptc.quality
+
+def dr2Selection(ptc):
+  return abs(ptc.deltaR2) < 0.25 #dr < 0.5
+
+def genMuInDetector(ptc):
+  if (abs(ptc.match.eta()) < barrelEta):
+    #It is OK if the momentum is high enough to not start spiralling
+    return (ptc.match.pt() > muonMinimumPtInBarrel)
+  else:
+    return (ptc.match.pt() > muonMinimumPtInEndcap)
+
+kinematicCutSelector = cfg.Analyzer(
+  Selector,
+  'kinematicCutSelector',
+  output = 'trigger_objects_in_detector',
+  input_objects = 'trigger_objects',
+  filter_func = genMuInDetector
+)
+
+muonQualityCut = cfg.Analyzer(
+  Selector,
+  'muonQualityCut',
+  output = 'good_trigger_objects',
+  input_objects = 'trigger_objects_in_detector',
+  filter_func = qualityCut
+)
+
+tightRestrictionMatchSelector = cfg.Analyzer(
+  Selector,
+  'tightRestrictionMatchSelector',
+  output = 'matched_trigger_object',
+  input_objects = 'good_trigger_objects',
+  filter_func = dr2Selection 
+)
+
+#ptBins = []
+#for x in xrange(0, 130, 10):
+#  ptBins.append(x)
+
+objectPtDistributionBinnedInMatchedObject = cfg.Analyzer(
+  MatchedObjectBinnedDistributions,
+  instance_label = 'objectPtDistributionBinnedInMatchedObject',
+  histo_name = 'objectPtDistributionBinnedInMatchedObject',
+  histo_title = 'p_{t}^{' + objectName + '} distribution binned in p^{' + matchedObjectName +'}_{t}',
+  matched_collection = 'matched_trigger_object',
+  binning = ptBins,
+  nbins = 200,
+  min = 0,
+  max = 100,
+  file_label = "tfile1",
+  plot_func = pt,
+  bin_func = pt,
+  log_y = False,
+  x_label = "p_{t}^{" + objectName + "} [GeV]",
+  y_label = "# events",
+)
+
+objectPtCumulativeDistributionBinnedInMatchedObject = cfg.Analyzer(
+  MatchedObjectBinnedCumulativeDistributions,
+  instance_label = 'objectPtCumulativeDistributionBinnedInMatchedObject',
+  histo_name = 'objectPtCumulativeDistributionBinnedInMatchedObject',
+  histo_title = 'p_{t}^{' + objectName + '} cumulative distribution binned in p^{' + matchedObjectName +'}_{t}',
+  matched_collection = 'matched_trigger_object',
+  binning = ptBins,
+  nbins = 200,
+  min = 0,
+  max = 100,
+  file_label = "tfile1",
+  plot_func = pt,
+  bin_func = pt,
+  log_y = False,
+  x_label = "p_{t}^{" + objectName + "} [GeV]",
+  y_label = "# events",
+  inverted = True
+)
+
+matchedObjectPtDistributionBinnedInMatchedObject = cfg.Analyzer(
+  MatchedObjectBinnedDistributions,
+  instance_label = 'matchedObjectPtDistributionBinnedInMatchedObject',
+  histo_name = 'matchedObjectPtDistributionBinnedInMatchedObject',
+  histo_title = 'p_{t}^{' + matchedObjectName + '} distribution binned in p^{' + matchedObjectName +'}_{t}',
+  matched_collection = 'matched_trigger_object',
+  binning = ptBins,
+  nbins = 200,
+  min = 0,
+  max = 200,
+  file_label = "tfile1",
+  plot_func = matchedParticlePt,
+  bin_func = pt,
+  log_y = False,
+  x_label = "p_{t}^{" + objectName + "} [GeV]",
+  y_label = "# events",
+)
+
+deltaPtDistributionBinnedInMatchedObject = cfg.Analyzer(
+  MatchedObjectBinnedDistributions,
+  instance_label = 'deltaPtDistributionBinnedInMatchedObject',
+  histo_name = 'deltaPtDistributionBinnedInMatchedObject',
+  histo_title = 'p_{t}^{' + objectName + '} - p_{t}^{' + matchedObjectName +'} distribution binned in p^{' + matchedObjectName +'}_{t}',
+  matched_collection = 'matched_trigger_object',
+  binning = ptBins,
+  nbins = 800,
+  min = -200,
+  max = 200,
+  file_label = "tfile1",
+  plot_func = deltaPt,
+  bin_func = pt,
+  log_y = False,
+  x_label = "p_{t}^{" + objectName + "} - p_{t}^{match} [GeV]",
+  y_label = "# events",
+)
+
+objectEtaDistributionBinnedInMatchedObject = cfg.Analyzer(
+  MatchedObjectBinnedDistributions,
+  instance_label = 'objectEtaDistributionBinnedInMatchedObject',
+  histo_name = 'objectEtaDistributionBinnedInMatchedObject',
+  histo_title = '#eta^{' + objectName + '} distribution binned in p^{' + matchedObjectName +'}_{t}',
+  matched_collection = 'matched_trigger_object',
+  binning = ptBins,
+  nbins = 200,
+  min = -10,
+  max = +10,
+  file_label = "tfile1",
+  plot_func = eta,
+  bin_func = pt,
+  log_y = False,
+  x_label = "#eta",
+  y_label = "# events"
+)
+
+matchedObjectEtaDistributionBinnedInMatchedObject = cfg.Analyzer(
+  MatchedObjectBinnedDistributions,
+  instance_label = 'matchedObjectEtaDistributionBinnedInMatchedObject',
+  histo_name = 'matchedObjectEtaDistributionBinnedInMatchedObject',
+  histo_title = '#eta^{' + matchedObjectName + '} distribution binned in p^{' + matchedObjectName +'}_{t}',
+  matched_collection = 'matched_trigger_object',
+  binning = ptBins,
+  nbins = 200,
+  min = -10,
+  max = +10,
+  file_label = "tfile1",
+  plot_func = matchedParticleEta,
+  bin_func = pt,
+  log_y = False,
+  x_label = "#eta",
+  y_label = "# events"
+)
+
+objectMatchedObjectPtRatioDistributionBinnedInMatchedObject = cfg.Analyzer(
+  MatchedObjectBinnedDistributions,
+  instance_label = 'objectMatchedObjectPtRatioDistributionBinnedInMatchedObject',
+  histo_name = 'objectMatchedObjectPtRatioDistributionBinnedInMatchedObject',
+  histo_title = 'p_{t}^{' + objectName + '}/p_{t}^{' + matchedObjectName +'} distribution binned in p^{' + matchedObjectName +'}_{t}',
+  matched_collection = 'matched_trigger_object',
+  binning = ptBins,
+  nbins = 800,
+  min = 0,
+  max = 20,
+  file_label = "tfile1",
+  plot_func = ptRatioWithMatched,
+  bin_func = pt,
+  log_y = False,
+  x_label = "p_{t}^{" + objectName + "}/p_{t}^{' + matchedObjectName +'}",
+  y_label = "# events"
+)
+
+deltaRDistributionBinnedInMatchedObject = cfg.Analyzer(
+  MatchedObjectBinnedDistributions,
+  instance_label = 'deltaRDistributionBinnedInMatchedObject',
+  histo_name = 'deltaRDistributionBinnedInMatchedObject',
+  histo_title = '#DeltaR distribution binned in p^{' + matchedObjectName +'}_{t}',
+  matched_collection = 'matched_trigger_object',
+  binning = ptBins,
+  nbins = 500,
+  min = 0,
+  max = 5,
+  file_label = "tfile1",
+  plot_func = deltaR,
+  bin_func = pt,
+  log_y = False,
+  x_label = "#DeltaR",
+  y_label = "# events"
+)
+
+objectPtDistribution = cfg.Analyzer(
+  Histogrammer,
+  'objectPtDistribution',
+  file_label = 'tfile1',
+  histo_name = 'objectPtDistribution',
+  histo_title = 'p_{t}^{' + objectName + '} distribution',
+  min = 0,
+  max = 100,
+  nbins = 200,
+  input_objects = 'matched_trigger_object',
+  value_func = pt,
+  x_label = "p_{t}^{" + objectName + "}",
+  y_label = "\# events"
+)
+
+objectQualityDistribution = cfg.Analyzer(
+  Histogrammer,
+  'objectQualityDistribution',
+  file_label = 'tfile1',
+  histo_name = 'objectQualityDistribution',
+  histo_title = 'Quality^{' + objectName + '} distribution',
+  min = 0,
+  max = 20,
+  nbins = 20,
+  input_objects = 'matched_trigger_object',
+  value_func = quality,
+  x_label = "Quality^{" + objectName + "}",
+  y_label = "\# events"
+)
+
+matchedObjectPtDistribution = cfg.Analyzer(
+  Histogrammer,
+  'matchedObjectPtDistribution',
+  file_label = 'tfile1',
+  histo_name = 'matchedObjectPtDistribution',
+  histo_title = 'p_{t}^{' + matchedObjectName + '} distribution',
+  min = 0,
+  max = 200,
+  nbins = 200,
+  input_objects = 'matched_trigger_object',
+  value_func = matchedParticlePt,
+  x_label = "p_{t}^{" + objectName + "}",
+  y_label = "\# events"
+)
+
+# definition of a sequence of analyzers,
+# the analyzers will process each event in this order
+sequence = cfg.Sequence( [
+  source,
+  kinematicCutSelector,
+  muonQualityCut,
+  tightRestrictionMatchSelector,
+  objectPtDistributionBinnedInMatchedObject,
+  objectMatchedObjectPtRatioDistributionBinnedInMatchedObject,
+  objectEtaDistributionBinnedInMatchedObject,
+  matchedObjectEtaDistributionBinnedInMatchedObject,
+  deltaRDistributionBinnedInMatchedObject,
+  deltaPtDistributionBinnedInMatchedObject,
+  objectPtDistribution,
+  objectQualityDistribution,
+  matchedObjectPtDistribution,
+  matchedObjectPtDistributionBinnedInMatchedObject,
+  objectPtCumulativeDistributionBinnedInMatchedObject,
+] )
+
+config = cfg.Config(
+  components = selectedComponents,
+  sequence = sequence,
+  services = [tfile_service_1],
+  events_class = Events
+)
+
+if __name__ == '__main__':
+
+  def next():
+      loop.process(loop.iEvent+1)
+
+  loop = Looper( 'looper', config,
+                 nEvents=100,
+                 nPrint=0,
+                 timeReport=True)
+  loop.process()
+  print loop.event
---
+++ genObjectToL1TObjectConvolutionCurves/computeEfficiencies.py
@@ -0,0 +1,111 @@
+from ROOT import TFile
+from ROOT import TH1F
+from ROOT import TCanvas
+from ROOT import TChain
+from numpy import zeros
+from numpy import float32
+import argparse
+import ast
+from bisect import bisect_right
+
+def computeEfficiencies(**kwargs):
+  chainGenObj = TChain (kwargs["GenObjTree"])
+  chainGenObj.Add(kwargs["GenObjFileFolder"]+"/*")
+
+  chainL1TObjGenObj = TChain(kwargs["MatchTree"])
+  chainL1TObjGenObj.Add(kwargs["MatchFileFolder"]+"/*")
+
+  bins = ast.literal_eval(kwargs["binning"])
+  detectorEta = float(kwargs["eta"])
+  quality = int(kwargs["quality"])
+  barrelEta = float(kwargs["barrelEta"])
+  minPtInBarrel = float(kwargs["minPtInBarrel"])
+  minPtInEndcap = float(kwargs["minPtInEndcap"])
+  
+  conversion_factors = []
+
+  numberOfGenObject = zeros(len(bins), float32)
+  numberOfMatchedObject = zeros(len(bins), float32)
+  conversion_factors = zeros(len(bins), float32)
+
+  nEntries = chainGenObj.GetEntries()
+  for entryIndex in xrange(0, nEntries):
+    #if entryIndex % 1000 == 0:
+    #  print entryIndex, "/", nEntries
+    chainGenObj.GetEntry(entryIndex)
+    genObj_pt = chainGenObj.genParticle_pt
+    genObj_eta = chainGenObj.genParticle_eta
+    isGoodMuon = False
+    if ((abs(genObj_eta) < barrelEta) and (genObj_pt > minPtInBarrel)):
+      isGoodMuon = True
+    # If not in barrel check if it is in the endcap acceptance
+    elif ((abs(genObj_eta) >= barrelEta) and (abs(genObj_eta) < detectorEta) and (genObj_pt > minPtInEndcap)): 
+      isGoodMuon = True
+    if not isGoodMuon: continue
+    # Looking for the correct bin
+    binNumber = bisect_right(bins, genObj_pt) - 1
+    if binNumber < 0:
+      print "Entry lost in underflow!" 
+      continue
+    numberOfGenObject[binNumber] += 1
+  
+  nEntries = chainL1TObjGenObj.GetEntries()
+  for entryIndex in xrange(0, nEntries):
+    #if entryIndex % 1000 == 0:
+    #  print entryIndex, "/", nEntries
+    chainL1TObjGenObj.GetEntry(entryIndex)
+    genObj_pt = chainL1TObjGenObj.genParticle_pt
+    genObj_eta = chainL1TObjGenObj.genParticle_eta
+    # There are two different pt threshold based on the muon eta
+    # If in barrel and the momentum is higher than the threshold, muon is good
+    isGoodMuon = False
+    if ((abs(genObj_eta) < barrelEta) and (genObj_pt > minPtInBarrel)):
+      isGoodMuon = True
+    # If not in barrel check if it is in the endcap acceptance
+    elif ((abs(genObj_eta) >= barrelEta) and (abs(genObj_eta) < detectorEta) and (genObj_pt > minPtInEndcap)): 
+      isGoodMuon = True
+    if not isGoodMuon: continue
+
+    l1tMuon_qual = chainL1TObjGenObj.l1tMuon_qual
+    # Looking for the correct bin
+    binNumber = bisect_right(bins, genObj_pt) - 1
+    if l1tMuon_qual < quality : 
+      continue
+    if binNumber < 0:
+      print "Entry lost in underflow!" 
+      continue
+    numberOfMatchedObject[binNumber] += 1
+
+  return numberOfMatchedObject, numberOfGenObject
+
+if __name__ == "__main__":
+
+  parser = argparse.ArgumentParser()
+
+  parser.add_argument('--GenObjTree', type=str)
+  parser.add_argument('--GenObjFileFolder', type=str)
+  parser.add_argument('--MatchTree', type=str)
+  parser.add_argument('--MatchFileFolder', type=str)
+  parser.add_argument('--binning', type=str)
+  parser.add_argument('--eta', type=float)
+  parser.add_argument('--quality', type=int)
+  parser.add_argument('--barrelEta', type=float)
+  parser.add_argument('--minPtInBarrel', type=float)
+  parser.add_argument('--minPtInEndcap', type=float)
+
+  args = parser.parse_args()
+
+  accepted, total = computeEfficiencies(
+    GenObjTree = args.GenObjTree,
+    GenObjFileFolder = args.GenObjFileFolder,
+    MatchTree = args.MatchTree,
+    MatchFileFolder = args.MatchFileFolder,
+    binning = args.binning,
+    eta = args.eta,
+    quality = args.quality,
+    barrelEta = args.barrelEta,
+    minPtInBarrel = args.minPtInBarrel,
+    minPtInEndcap = args.minPtInEndcap,
+  )
+  factors = accepted/total
+  print "Conversion factors are", factors
